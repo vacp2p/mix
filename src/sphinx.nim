6da6d220fb0c712fa4a23c4a6c4ea99d2096431a
akshaya <AkshayaMani@users.noreply.github.com>
2024-08-09 18:15:55 -0400

    Add manual serialization

--- src/sphinx.nim
+++ src/sphinx.nim
@@ -1,17 +1,15 @@
-import crypto, curve25519, sphinx_pb
-import nimcrypto, std/math, sequtils
+import config, crypto, curve25519, serialization
+import nimcrypto, std/math, sequtils 
 
-const k = 16 # Security parameter
-const r = 5 # Maximum path length
-const t = 3 # t.k - combined length of next hop address and delay
-const lambda = 500 # Parameter for exp distribution for generating random delay 
+# const lambda* = 500 # Parameter for exp distribution for generating random delay
 
 # Function to compute alphas, shared secrets, and blinders
-proc computeSharedSecrets(publicKeys: openArray[FieldElement]): tuple[headerInitialsArr: seq[HeaderInitials], errorMsg: string] =
+proc computeAlpha(publicKeys: openArray[FieldElement]): tuple[alpha_0: seq[byte], s: seq[seq[byte]], errorMsg: string] =
     if publicKeys.len == 0:
-        return (@[], "No public keys provided")
+        return (@[], @[@[]], "No public keys provided")
 
-    var tuples: seq[HeaderInitials] = newSeq[HeaderInitials](publicKeys.len)
+    var s: seq[seq[byte]] = newSeq[seq[byte]](publicKeys.len)
+    var alpha_0: seq[byte] 
     var alpha: FieldElement
     var secret: FieldElement
     var blinders: seq[FieldElement] = @[]
@@ -20,24 +18,22 @@ proc computeSharedSecrets(publicKeys: openArray[FieldElement]): tuple[headerInit
 
     for i in 0..<publicKeys.len:
         if publicKeys[i].len != FieldElementSize:
-            return (@[], "Invalid public key " & $i)
+            return (@[], @[@[]], "Invalid public key " & $i)
 
         # Compute alpha, shared secret, and blinder
         if i == 0:
             alpha = multiplyBasePointWithScalars(@[x])
             secret = multiplyPointWithScalars(publicKeys[0], @[x])
+            alpha_0 = fieldElementToBytes(alpha)
         else:
             alpha = multiplyPointWithScalars(alpha, @[blinders[i - 1]])
             secret = multiplyPointWithScalars(publicKeys[i], blinders) # ToDo: Optimize point multiplication by multiplying scalars first
 
         blinders.add(bytesToFieldElement(sha256_hash(fieldElementToBytes(alpha) & fieldElementToBytes(secret))))
 
-        tuples[i] = HeaderInitials()  
-        tuples[i].Alpha = fieldElementToBytes(alpha)
-        tuples[i].Secret = fieldElementToBytes(secret)
-        tuples[i].Blinder = fieldElementToBytes(blinders[i])
+        s[i] = fieldElementToBytes(secret)
 
-    return (tuples, "")
+    return (alpha_0, s, "")
 
 # Helper function to derive key material
 proc deriveKeyMaterial(keyName: string, s: seq[byte]): seq[byte] =
@@ -59,6 +55,7 @@ proc computeFillerStrings(s: seq[seq[byte]]): seq[byte] =
         filler = aes_ctr(aes_key, iv, filler & zeroPadding)
     return filler
 
+#[
 # ToDo: Replace with better implementation for production
 # Helper function to generate a random 16-bit delay
 proc generateRandomDelay(): seq[byte] =
@@ -78,12 +75,17 @@ proc generateRandomDelay(): seq[byte] =
   delayBytes[0] = byte(cappedDelay and 0xFF)
   delayBytes[1] = byte((cappedDelay shr 8) and 0xFF)
   return toseq(delayBytes)
-  
-proc computeBetasGammasDeltas(s: seq[seq[byte]], filler: seq[byte], address: openArray[string], message: seq[byte]): tuple[betas: seq[seq[byte]], gammas: seq[seq[byte]]] = 
+]#
+
+# Function to compute betas, gammas, and deltas  
+proc computeBetaGammaDelta(s: seq[seq[byte]], hop: openArray[Hop], msg: Message, delay: seq[byte]): tuple[beta, gamma, delta: seq[byte]] = 
     let sLen = s.len
-    var betas: seq[seq[byte]] = newSeq[seq[byte]](sLen)
-    var gammas: seq[seq[byte]] = newSeq[seq[byte]](sLen)
-    var deltas: seq[seq[byte]] = newSeq[seq[byte]](sLen)
+    var beta: seq[byte]
+    var gamma: seq[byte]
+    var delta: seq[byte]
+
+    # Compute filler strings
+    let filler = computeFillerStrings(s)
 
     for i in countdown(sLen-1, 0):
         # Derive AES keys, MAC key, and IVs
@@ -94,35 +96,37 @@ proc computeBetasGammasDeltas(s: seq[seq[byte]], filler: seq[byte], address: ope
         let delta_aes_key = kdf(deriveKeyMaterial("delta_aes_key", s[i]))
         let delta_iv = kdf(deriveKeyMaterial("delta_iv", s[i]))
 
-        # Generate Random Delay
-        var delayBytes = generateRandomDelay()
-
         # Compute Beta and Gamma
         if i == sLen - 1:
-            var fillerLength: int
+            var paddingLength: int
             var zeroPadding: seq[byte]
 
-            fillerLength = ((2 * (r - sLen)) + t + 2) * k
-            zeroPadding = newSeq[byte](fillerLength)
-            betas.add(aes_ctr(beta_aes_key, beta_iv, zeroPadding) & filler)
+            paddingLength = ((2 * (r - sLen)) + t + 2) * k
+            zeroPadding = newSeq[byte](paddingLength)
+            beta = aes_ctr(beta_aes_key, beta_iv, zeroPadding) & filler
 
-            fillerLength = k
-            zeroPadding = newSeq[byte](fillerLength)
-            deltas.add(aes_ctr(delta_aes_key, delta_iv, zeroPadding & message)) 
+            delta = aes_ctr(delta_aes_key, delta_iv, serializeMessage(msg)) 
         else:
-            let addrBytes = @(address[i+1].toOpenArrayByte(0, address[i+1].high))
-            betas.add(aes_ctr(beta_aes_key, beta_iv, addrBytes & delayBytes & gammas[i+1] & betas[i+1][0..(((2 * r) - 1) * k) - 1]))
+            let routingInfo = initRoutingInfo(hop[i+1], delay, gamma, beta[0..(((2 * r) - 1) * k) - 1])
+            beta = aes_ctr(beta_aes_key, beta_iv, serializeRoutingInfo(routingInfo))
 
-            deltas.add(aes_ctr(delta_aes_key, delta_iv, deltas[i+1]))
+            delta = aes_ctr(delta_aes_key, delta_iv, delta)
 
-        gammas.add(toseq(hmac(mac_key, betas[i])))
+        gamma = toseq(hmac(mac_key, beta))
 
-    return (betas, gammas)
+    return (beta, gamma, delta)
 
-proc wrapInSphinxPacket*(message: seq[byte], publicKeys: openArray[FieldElement]): SphinxPacket =
-    # Create HeaderInitials, Header, and handle any errors
-    let (headerInitials, errMsg) = computeSharedSecrets(publicKeys)
+proc wrapInSphinxPacket*(message: seq[byte], publicKeys: openArray[FieldElement], delay: seq[byte], hop: openArray[Hop], msg: Message): SphinxPacket =
+    # Compute PoW
+
+
+    # Compute alphas and shared secrets
+    let (alpha_0, s, errMsg) = computeAlpha(publicKeys)
     if errMsg.len > 0:
         echo "Error in createSphinxHeader: ", errMsg
         return SphinxPacket()
-    return SphinxPacket()
+
+    # Compute betas, gammas, and deltas
+    let (beta_0, gamma_0, delta_0) = computeBetaGammaDelta(s, hop, msg, delay)
+    return initSphinxPacket(initHeader(alpha_0, beta_0, gamma_0), delta_0)
+
