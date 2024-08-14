3f3f1ce9a47515663f49f6caf757288afd640cd9
akshaya <AkshayaMani@users.noreply.github.com>
2024-08-14 11:20:11 -0400

    Add path length

--- src/config.nim
+++ src/config.nim
@@ -2,6 +2,7 @@ const
   k* = 16 # Security parameter 
   r* = 5 # Maximum path length
   t* = 3 # t.k - combined length of next hop address and delay
+  L* = 3 # Path length
   powSize* = 12 # 4 byte nonce + 8 byte timestamp
   alphaSize* = 32 # Group element
   betaSize* = 208 # (2r+t)k bytes
