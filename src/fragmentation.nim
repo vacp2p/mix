2d0f69859fead80b4782b1014ca68803db3a502b
akshaya <AkshayaMani@users.noreply.github.com>
2024-10-18 21:57:20 -0400

    Add fragmentation

---
+++ src/fragmentation.nim
@@ -0,0 +1,71 @@
+import config, seqno_generator, utils
+import libp2p/peerid
+
+const paddingLengthSize* = 2
+const seqNoSize* = 4
+const dataSize* = messageSize - paddingLengthSize - seqNoSize
+
+type
+  MessageChunk* = object
+    paddingLength: uint16
+    data: seq[byte]
+    seqNo: uint32 
+
+proc initMessageChunk*(paddingLength: uint16, data: seq[byte], seqNo: uint32 ): MessageChunk =
+  result.paddingLength = paddingLength
+  result.data = data
+  result.seqNo = seqNo
+
+proc getMessageChunk*(msgChunk: MessageChunk): (uint16, seq[byte], uint32 ) =
+  (msgChunk.paddingLength, msgChunk.data, msgChunk.seqNo)
+
+proc serializeMessageChunk*(msgChunk: MessageChunk): seq[byte] = 
+  let paddingBytes = uint16ToBytes(msgChunk.paddingLength)
+  let seqNoBytes = uint32ToBytes(msgChunk.seqNo)
+  assert len(msgChunk.data) == dataSize, "Padded data must be exactly " & $dataSize & " bytes"
+  result = paddingBytes & msgChunk.data & seqNoBytes
+
+proc deserializeMessageChunk*(data: openArray[byte]): MessageChunk =
+    assert len(data) == messageSize, "Data must be exactly " & $messageSize & " bytes"
+
+    result.paddingLength = bytesToUInt16(data[0..paddingLengthSize - 1])
+    result.data = data[paddingLengthSize..(paddingLengthSize + dataSize - 1)]
+    result.seqNo = bytesToUInt32(data[paddingLengthSize + dataSize..^1])
+
+proc ceilDiv*(a, b: int): int =
+  (a + b - 1) div b
+
+proc padAndChunkMessage*(message: string, peerId: PeerId): seq[MessageChunk] =
+  let messageBytes = cast[seq[byte]](message)
+  var seqNoGen = initSeqNo(peerId)
+  seqNoGen.generateSeqNo(messageBytes)
+
+  var chunks: seq[MessageChunk] = @[]
+
+  # Split to chunks
+  let totalChunks = max(1, ceilDiv(len(messageBytes), dataSize)) # Ensure at least one chunk is generated
+  for i in 0..<totalChunks:
+    let startIdx = i * dataSize
+    let endIdx = min(startIdx + dataSize, len(messageBytes))
+    let chunkData = messageBytes[startIdx..endIdx - 1]
+
+    # Calculate padding length
+    let paddingLength = uint16(dataSize - len(chunkData))
+
+    let paddedData = if paddingLength > 0:
+      # Create padding bytes
+      let paddingBytes = newSeq[byte](paddingLength)
+      paddingBytes & chunkData
+    else:
+      chunkData
+
+    let msgChunk = initMessageChunk(paddingLength, paddedData, seqNoGen.getSeqNo())
+    chunks.add(msgChunk)
+
+    seqNoGen.incSeqNo()
+
+  return chunks
+
+# ToDo: Unpadding and reassembling messages will be handled by the top-level applications.
+# Although padding and splitting messages could also be managed at that level, we implement it here to clarify the sender's logic.
+# This is crucial as the sender is responsible for wrapping messages in Sphinx packets.
---
+++ tests/test_fragmentation.nim
@@ -0,0 +1,85 @@
+import strutils, unittest
+import libp2p/peerid
+import ../src/config, ../src/fragmentation
+
+suite "Fragmentation":
+  let peerId = PeerId.init("16Uiu2HAmFkwLVsVh6gGPmSm9R3X4scJ5thVdKfWYeJsKeVrbcgVC").get()
+
+  test "serialize_deserialize_message_chunk":
+    let message = repeat('A', dataSize)
+    let chunks = padAndChunkMessage(message, peerId)
+    let (paddingLength, data, seqNo) = getMessageChunk(chunks[0])
+
+    let serialized = serializeMessageChunk(chunks[0])
+    let deserialized = deserializeMessageChunk(serialized)
+    let (dPaddingLength, dData, dSeqNo) = getMessageChunk(deserialized)
+
+    assert paddingLength == dPaddingLength, "Padding length not equal to original padding length."
+    assert data == dData, "Data not equal to original data."
+    assert seqNo == dSeqNo, "Sequence no. not equal to original sequence no."
+
+  test "pad_and_chunk_small_message":
+    let message = "Hello, World!"
+    let messageBytesLen = len(cast[seq[byte]](message))
+
+    let chunks = padAndChunkMessage(message, peerId)
+    let totalChunks = max(1, ceilDiv(messageBytesLen, dataSize))
+    assert chunks.len == totalChunks, "Chunk length must be " & $totalChunks & "."
+
+    let (paddingLength, data, _) = getMessageChunk(chunks[0])
+    assert paddingLength == uint16(dataSize - messageBytesLen), "Padding length must be exactly " & $(dataSize - messageBytesLen) & " bytes." 
+    assert len(data) == dataSize, "Padded data must be exactly " & $dataSize & " bytes."
+
+  test "pad_and_chunk_large_message":
+    let message = repeat('A', messageSize * 2 + 10)
+    let messageBytesLen = len(cast[seq[byte]](message))
+
+    let chunks = padAndChunkMessage(message, peerId)
+    let totalChunks = max(1, ceilDiv(messageBytesLen, dataSize))
+    assert chunks.len == totalChunks, "Chunk length must be " & $totalChunks & "."
+
+    for i in 0..<totalChunks:
+        let (paddingLength, data, _) = getMessageChunk(chunks[i])
+        if i != totalChunks - 1:
+            assert paddingLength == 0, "Padding length must be zero."
+        else:
+            let chunkSize = messageBytesLen mod dataSize
+            assert paddingLength == uint16(dataSize - chunkSize), "Padding length must be exactly " & $(dataSize - chunkSize) & " bytes."
+        assert len(data) == dataSize, "Padded data must be exactly " & $dataSize & " bytes."
+
+  test "chunk_sequence_numbers_are_consecutive":
+    let message = repeat('A', messageSize * 3)
+    let messageBytesLen = len(cast[seq[byte]](message))
+
+    let chunks = padAndChunkMessage(message, peerId)
+    let totalChunks = max(1, ceilDiv(messageBytesLen, dataSize))
+    assert chunks.len == totalChunks, "Chunk length must be " & $totalChunks & "."
+
+    let (_, _, firstSeqNo) = getMessageChunk(chunks[0])
+
+    for i in 1..<totalChunks:
+        let (_, _, seqNo) = getMessageChunk(chunks[i])
+        assert seqNo == firstSeqNo + uint32(i), "Sequence number of chunks not consecutive."
+
+  test "chunk_data_reconstructs_original_message":
+    let message = "This is a test message that will be split into multiple chunks."
+    let chunks = padAndChunkMessage(message, peerId)
+    var reconstructed: seq[byte]
+    for chunk in chunks:
+      let (paddingLength, data, _) = getMessageChunk(chunk)
+      reconstructed.add(data[paddingLength.int..^1])
+    check cast[string](reconstructed) == message
+
+  test "empty_message_handling":
+    let message = ""
+    let chunks = padAndChunkMessage(message, peerId)
+    assert chunks.len == 1, "Chunk length must be 1."
+    let (paddingLength, _, _) = getMessageChunk(chunks[0])
+    assert paddingLength == uint16(dataSize), "Padding length must be exactly " & $(dataSize) & " bytes."
+
+  test "message_size_equal_to_chunk_size":
+    let message = repeat('A', dataSize)
+    let chunks = padAndChunkMessage(message, peerId)
+    assert chunks.len == 1, "Chunk length must be 1."
+    let (paddingLength, _, _) = getMessageChunk(chunks[0])
+    assert paddingLength == 0, "Padding length must be zero."
