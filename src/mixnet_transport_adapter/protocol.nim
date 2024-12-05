e071cedd1170f5f0dc23e64a0c9bc0ef76124c0f
akshaya <AkshayaMani@users.noreply.github.com>
2024-12-05 12:19:56 -0500

    Migrated protocol type and related functions

---
+++ src/mixnet_transport_adapter/protocol.nim
@@ -0,0 +1,35 @@
+import chronos, std/enumerate
+import libp2p/[builders, protocols/ping, stream/connection]
+
+const protocolTypeSize* = 2
+
+type ProtocolType* = enum
+  Ping = 0 # Represents the Ping protocol
+  GossipSub = 1 # Represents the GossipSub protocol
+  OtherProtocol = 2 # Placeholder for other protocols
+
+type ProtocolHandler* =
+  proc(switch: Switch, conn: Connection, proto: ProtocolType): Future[void] {.async.}
+
+proc protocolFromString*(proto: string): ProtocolType =
+  case proto
+  of PingCodec:
+    return Ping
+  else:
+    return OtherProtocol
+
+proc stringFromProtocol*(proto: ProtocolType): string =
+  case proto
+  of Ping:
+    return PingCodec
+  else:
+    return "other"
+
+method callHandler*(
+    switch: Switch, conn: Connection, proto: ProtocolType
+): Future[void] {.base, async.} =
+  let codec = stringFromProtocol(proto)
+  for index, handler in enumerate(switch.ms.handlers):
+    if codec in handler.protos:
+      await handler.protocol.handler(conn, codec)
+      return
