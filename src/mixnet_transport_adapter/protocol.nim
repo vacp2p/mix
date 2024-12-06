b3105600db464b7bee3ada31ee2f124a7f455506
akshaya <AkshayaMani@users.noreply.github.com>
2024-12-06 13:31:44 -0500

    Added `NoRespPing` related changes

--- src/mixnet_transport_adapter/protocol.nim
+++ src/mixnet_transport_adapter/protocol.nim
@@ -1,12 +1,14 @@
 import chronos, std/enumerate
 import libp2p/[builders, protocols/ping, stream/connection]
+import ../protocols/[noresp_ping]
 
 const protocolTypeSize* = 2
 
 type ProtocolType* = enum
   Ping = 0 # Represents the Ping protocol
   GossipSub = 1 # Represents the GossipSub protocol
-  OtherProtocol = 2 # Placeholder for other protocols
+  NoRespPing = 2 # Represents the custom NoRespPing protocol
+  OtherProtocol = 3 # Placeholder for other protocols
 
 type ProtocolHandler* =
   proc(conn: Connection, proto: ProtocolType): Future[void] {.async.}
@@ -15,6 +17,8 @@ proc protocolFromString*(proto: string): ProtocolType =
   case proto
   of PingCodec:
     return Ping
+  of NoRespPingCodec:
+    return NoRespPing
   else:
     return OtherProtocol
 
@@ -22,6 +26,8 @@ proc stringFromProtocol*(proto: ProtocolType): string =
   case proto
   of Ping:
     return PingCodec
+  of NoRespPing:
+    return NoRespPingCodec
   else:
     return "other"
 
