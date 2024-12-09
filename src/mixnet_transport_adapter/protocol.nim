53900c0f7f143c914cb0f44314d76fd94fe1bf77
akshaya <AkshayaMani@users.noreply.github.com>
2024-12-09 11:55:42 -0500

    Changed to enum that uses the protocol codecs directly

--- src/mixnet_transport_adapter/protocol.nim
+++ src/mixnet_transport_adapter/protocol.nim
@@ -1,40 +1,30 @@
-import chronos, std/enumerate
-import libp2p/[builders, protocols/ping, stream/connection]
+import chronos, std/enumerate, strutils
+import libp2p/[builders, protocols/ping, protocols/pubsub/gossipsub/types, stream/connection]
 import ../protocols/[noresp_ping]
 
 const protocolTypeSize* = 2
 
 type ProtocolType* = enum
-  Ping = 0 # Represents the Ping protocol
-  GossipSub = 1 # Represents the GossipSub protocol
-  NoRespPing = 2 # Represents the custom NoRespPing protocol
-  OtherProtocol = 3 # Placeholder for other protocols
+  Ping = PingCodec
+  GossipSub12 = GossipSubCodec_12
+  GossipSub11 = GossipSubCodec_11
+  GossipSub10 = GossipSubCodec_10
+  NoRespPing = NoRespPingCodec
+  OtherProtocol = "other" # Placeholder for other protocols
 
 type ProtocolHandler* =
   proc(conn: Connection, proto: ProtocolType): Future[void] {.async.}
 
-proc protocolFromString*(proto: string): ProtocolType =
-  case proto
-  of PingCodec:
-    return Ping
-  of NoRespPingCodec:
-    return NoRespPing
-  else:
-    return OtherProtocol
-
-proc stringFromProtocol*(proto: ProtocolType): string =
-  case proto
-  of Ping:
-    return PingCodec
-  of NoRespPing:
-    return NoRespPingCodec
-  else:
-    return "other"
+proc fromString*(T: type ProtocolType, proto: string): ProtocolType =
+  try:
+    parseEnum[ProtocolType](proto)
+  except ValueError:
+    ProtocolType.OtherProtocol
 
 method callHandler*(
     switch: Switch, conn: Connection, proto: ProtocolType
 ): Future[void] {.base, async.} =
-  let codec = stringFromProtocol(proto)
+  let codec = $proto
   for index, handler in enumerate(switch.ms.handlers):
     if codec in handler.protos:
       await handler.protocol.handler(conn, codec)
