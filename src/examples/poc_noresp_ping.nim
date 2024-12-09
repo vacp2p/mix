1a5016b4f7314bdb1412d1aade1f8b34b6b11f51
AkshayaMani <AkshayaMani@users.noreply.github.com>
2024-12-09 10:55:18 -0500

    Update src/examples/poc_noresp_ping.nim
    Co-authored-by: richΛrd <info@richardramos.me>

--- src/examples/poc_noresp_ping.nim
+++ src/examples/poc_noresp_ping.nim
@@ -121,7 +121,7 @@ proc mixnet_with_transport_adapter_poc() {.async.} =
   let
     mixTransport = nodes[senderIndex].transports[transportIndex]
     peerId = nodes[receiverIndex].peerInfo.peerId
-    peerIdOpt = Opt[PeerId](oResultPrivate: true, vResultPrivate: peerId)
+    peerIdOpt = Opt[PeerId].some(peerId)
 
   let pingFuture = newFuture[void]()
 
