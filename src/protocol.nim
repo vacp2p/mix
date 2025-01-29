90d7070dfb7bbe49b66a5eb13b49c43a02a81c34
akshaya <AkshayaMani@users.noreply.github.com>
2025-01-29 01:50:02 -0500

    Update protocol
    - Added NoRespPing codec
    - Added protocol handler callback

--- src/protocol.nim
+++ src/protocol.nim
@@ -1,6 +1,7 @@
-import chronos, strutils
+import chronos, std/enumerate, strutils
 import
   libp2p/[builders, protocols/ping, protocols/pubsub/gossipsub/types, stream/connection]
+import protocols/noresp_ping
 
 const protocolTypeSize* = 2
 
@@ -9,6 +10,7 @@ type ProtocolType* = enum
   GossipSub12 = GossipSubCodec_12
   GossipSub11 = GossipSubCodec_11
   GossipSub10 = GossipSubCodec_10
+  NoRespPing = NoRespPingCodec
   OtherProtocol = "other" # Placeholder for other protocols
 
 type ProtocolHandler* =
@@ -19,3 +21,13 @@ proc fromString*(T: type ProtocolType, proto: string): ProtocolType =
     parseEnum[ProtocolType](proto)
   except ValueError:
     ProtocolType.OtherProtocol
+
+method callHandler*(
+    switch: Switch, conn: Connection, proto: ProtocolType
+): Future[void] {.base, async.} =
+  let codec = $proto
+  for index, handler in enumerate(switch.ms.handlers):
+    if codec in handler.protos:
+      await handler.protocol.handler(conn, codec)
+      return
+  error "Handler doesn't exist", codec = codec
