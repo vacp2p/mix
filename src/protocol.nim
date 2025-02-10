c8e2d3feeeb9c17f611a0664913fa2c86155ff43
akshaya <AkshayaMani@users.noreply.github.com>
2025-02-09 21:33:05 -0500

    Removed protocol handler callback

--- src/protocol.nim
+++ src/protocol.nim
@@ -1,6 +1,7 @@
 import chronos, strutils
 import
   libp2p/[builders, protocols/ping, protocols/pubsub/gossipsub/types, stream/connection]
+import ./protocols/noresp_ping
 
 const protocolTypeSize* = 2
 
@@ -9,10 +10,8 @@ type ProtocolType* = enum
   GossipSub12 = GossipSubCodec_12
   GossipSub11 = GossipSubCodec_11
   GossipSub10 = GossipSubCodec_10
+  NoRespPing = NoRespPingCodec
   OtherProtocol = "other" # Placeholder for other protocols
-
-type ProtocolHandler* =
-  proc(conn: Connection, proto: ProtocolType): Future[void] {.async.}
 
 proc fromString*(T: type ProtocolType, proto: string): ProtocolType =
   try:
