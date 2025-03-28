e2ca7478dcbe32929f7438c6e48ac9ce51f9ff0e
Prem Chaitanya Prathi <chaitanyaprem@gmail.com>
2025-03-28 14:00:21 +0530

    uncomment waku relative import

--- src/app_protocols.nim
+++ src/app_protocols.nim
@@ -2,7 +2,7 @@ import chronos, strutils
 import
   libp2p/[builders, protocols/ping, protocols/pubsub/gossipsub/types, stream/connection]
 import ./protocols/noresp_ping
-#import ../../../waku/waku_core/codecs  #TODO: change this to import the correct path
+import ../../../waku/waku_core/codecs  #TODO: change this to import the correct path
 
 const protocolTypeSize* = 2
 
