beb7264848c724e5fe398e51199d9e9f081bc6ba
Prem Chaitanya Prathi <chaitanyaprem@gmail.com>
2025-04-01 17:56:10 +0530

    use counter instead of gauge

--- mix/mix_metrics.nim
+++ mix/mix_metrics.nim
@@ -2,11 +2,11 @@
 
 import metrics
 
-declarePublicGauge mix_messages_recvd,
+declarePublicCounter mix_messages_recvd,
   "number of mix messages received", ["type"]
 
-declarePublicGauge mix_messages_forwarded,
+declarePublicCounter mix_messages_forwarded,
   "number of mix messages forwarded", ["type"]
 
-declarePublicGauge mix_messages_error,
+declarePublicCounter mix_messages_error,
   "number of mix messages failed processing",  ["type", "error"]
