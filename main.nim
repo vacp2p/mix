a3a80ff84878b812a5b924803d81a0e85c24e66a
Ben-PH <benphawke@gmail.com>
2025-06-11 18:19:32 +0200

    refactor: launch main.nim locally, and using cli instead of env-vars

--- main.nim
+++ main.nim
@@ -40,6 +40,7 @@ proc createSwitch(id, port: int, isMix: bool, filePath: string): Switch =
       discard initializeMixNodes(1, port)
 
       let mixNodeInfo = getMixNodeInfo(mixNodes[0])
+      echo $mixNodeInfo
       multiAddrStr = mixNodeInfo[0]
       libp2pPubKey = mixNodeInfo[3]
       libp2pPrivKey = mixNodeInfo[4]
@@ -67,15 +68,9 @@ proc createSwitch(id, port: int, isMix: bool, filePath: string): Switch =
       warn "Failed to set up node", nodeId = id
       return
 
-    let addresses = getInterfaces().filterIt(it.name == "eth0").mapIt(it.addresses)
-    if addresses.len < 1 or addresses[0].len < 1:
-      error "Can't find local ip!"
-      return
-
     let
-      externalAddr = ($addresses[0][0].host).split(":")[0]
       peerId = switch.peerInfo.peerId
-      externalMultiAddr = fmt"/ip4/{externalAddr}/tcp/{port}/p2p/{peerId}"
+      externalMultiAddr = fmt"/ip4/127.0.0.1/tcp/{port}/p2p/{peerId}"
 
     if isMix:
       discard initMixMultiAddrByIndex(0, externalMultiAddr)
@@ -131,35 +126,45 @@ func byteToHex(b: byte): string =
   b.toHex(2)
 
 proc main() {.async.} =
+  let args = commandLineParams()
+  echo "args: {id} {count} {rate} {size} {pub_count} {mix_count} {conn to}"
   randomize()
 
+  let 
+    myId = parseInt(args[0])
+    hostname = fmt "node-{myId}"
+  info "Hostname", host = hostname
+    # -e NODES="$N" \
+    # -e MSGRATE=10 \
+    # -e MSGSIZE=20 \
+    # -e PUBLISHERS=5 \
+    # -e CONNECTTO=4 \
+    # -e LOG_LEVEL=TRACE \
   let
-    hostname = getHostname()
-    node_count = parseInt(getEnv("NODES"))
-    msg_rate = parseInt(getEnv("MSGRATE"))
-    msg_size = parseInt(getEnv("MSGSIZE"))
-    publisherCount = parseInt(getEnv("PUBLISHERS"))
-    mixCount = publisherCount # Publishers will be the mix nodes for now
-    connectTo = parseInt(getEnv("CONNECTTO"))
-    filePath = getEnv("FILEPATH", "./")
+    node_count = parseInt(args[1]) # parseInt(getEnv("NODES"))
+    msg_rate = parseInt(args[2]) # parseInt(getEnv("MSGRATE"))
+    msg_size = parseInt(args[3]) # parseInt(getEnv("MSGSIZE"))
+    publisherCount = parseInt(args[4]) # parseInt(getEnv("PUBLISHERS"))
+    mixCount = publisherCount # publisherCount # Publishers will be the mix nodes for now
+    connectTo = parseInt(args[5]) # parseInt(getEnv("CONNECTTO"))
+    filePath = "./infos"
     rng = libp2p.newRng()
 
   if publisherCount > node_count:
     error "Publisher count is greater than total node count"
     return
 
-  info "Hostname", host = hostname
-  let myId = getHostname().split('-')[^1].parseInt()
-  info "ID", id = myId
 
   let
     isPublisher = myId < publisherCount
       # [0..<publisherCount] contains all the publishers
     isMix = isPublisher # Publishers will be the mix nodes for now
-    myport = parseInt(getEnv("PORT", "5000"))
+    myport = 50000 + myId
     switch = createSwitch(myId, myport, isMix, filePath)
 
-  await sleepAsync(10.seconds)
+  info "port", pt = myport
+
+  await sleepAsync(5.seconds)
 
   var gossipSub: GossipSub
 
@@ -278,6 +283,7 @@ proc main() {.async.} =
     let pubInfo = readPubInfoFromFile(i, filePath / fmt"libp2pPubInfo").expect(
         "should be able to read pubinfo"
       )
+    echo fmt "{pubInfo}"
     let (multiAddr, _) = getPubInfo(pubInfo)
     let ma = MultiAddress.init(multiAddr).expect("should be a multiaddr")
     addrs.add ma
@@ -291,7 +297,7 @@ proc main() {.async.} =
       try:
         info "Trying to connect", addrs = addrs[index]
         let peerId =
-          await switch.connect(addrs[index], allowUnknownPeerId = true).wait(5.seconds)
+          await switch.connect(addrs[index], allowUnknownPeerId = true).wait(20.seconds)
         connected.inc()
         index.inc()
         info "Connected!"
@@ -307,8 +313,8 @@ proc main() {.async.} =
 
   info "Publishing turn", id = myId
 
-  let count = 50
-  for msg in high(int) - count ..< high(int): #client.param(int, "message_count"):
+  let count = 2
+  for msg in 0 ..< 2: #client.param(int, "message_count"):
     if msg mod publisherCount == myId:
       # info "Sending message", time = times.getTime()
       let now = getTime()
